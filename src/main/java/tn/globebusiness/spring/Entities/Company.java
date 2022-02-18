b30213e75f826205c870e6527c051a060a11d886
Fedi-Said-Ghourabi <fedisaid.ghourabi@esprit.tn>
2022-02-18 02:04:05 +0100

    Added Employee, Company and Gender entities

---
+++ src/main/java/tn/globebusiness/spring/Entities/Company.java
@@ -0,0 +1,38 @@
+package tn.globebusiness.spring.Entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "company")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "domain")
+    private String domain;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private Long phone;
+
+    @Column(name = "image")
+    private String image;
+
+}
---
+++ src/main/java/tn/globebusiness/spring/Entities/Employee.java
@@ -0,0 +1,46 @@
+package tn.globebusiness.spring.Entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private Long phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "image")
+    private String image;
+
+}
