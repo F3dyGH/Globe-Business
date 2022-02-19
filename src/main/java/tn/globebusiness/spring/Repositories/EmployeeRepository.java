fcdcaa7aaae38adbd2ff73dad67c81195bdf7c07
Fedi-Said-Ghourabi <fedisaid.ghourabi@esprit.tn>
2022-02-19 05:27:17 +0100

    Find by name Query Method

--- src/main/java/tn/globebusiness/spring/Repositories/EmployeeRepository.java
+++ src/main/java/tn/globebusiness/spring/Repositories/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package tn.globebusiness.spring.Repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tn.globebusiness.spring.Entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    @Query("select e from Employee e where e.name =?1")
+    Employee findByName(String name);
+
 }
