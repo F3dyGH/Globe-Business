c72087d5563dbce2b941fcf887d93bef0ae2be51
dhamer ayoub <ayoubdhamer5@gmail.com>
2022-03-01 02:17:44 +0100

    event + email service

--- src/main/java/tn/globebusiness/spring/GlobeBusinessApplication.java
+++ src/main/java/tn/globebusiness/spring/GlobeBusinessApplication.java
@@ -1,13 +1,75 @@
 package tn.globebusiness.spring;
 
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 @SpringBootApplication
 public class GlobeBusinessApplication {
+	
+	@Autowired
+	JavaMailSender mailSender;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobeBusinessApplication.class, args);
 	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
 
+		sendEmailWithAttachment("ayoubdhamer5@gmail.com",
+				"This is Email Body with Attachment...",
+				"This email has attachment",
+				"D:\\ayoub\\Downloads\\images.jpg");
+		
+		/*sendSimpleEmail("ayoubdhamer5@gmail.com",
+				"This is the Email Body",
+				"This is the email Subject");*/
+		
+	}
+
+	public void sendSimpleEmail(String toEmail, String body, String subject) {
+		SimpleMailMessage message = new SimpleMailMessage();
+
+		message.setFrom("ayoubdhamer5@gmail.com");
+		message.setTo(toEmail);
+		message.setText(body);
+		message.setSubject(subject);
+
+		mailSender.send(message);
+		System.out.println("Mail Send...");
+	}
+	
+	public void sendEmailWithAttachment(String toEmail, String body, String subject, String attachment)
+			throws MessagingException {
+
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+		mimeMessageHelper.setFrom("ayoubdhamer5@gmail.com");
+		mimeMessageHelper.setTo(toEmail);
+		mimeMessageHelper.setText(body);
+		mimeMessageHelper.setSubject(subject);
+
+		FileSystemResource fileSystem = new FileSystemResource(new File(attachment));
+
+		mimeMessageHelper.addAttachment(fileSystem.getFilename(), fileSystem);
+
+		mailSender.send(mimeMessage);
+		System.out.println("Mail Send...");
+
+	}
 }
--- src/main/java/tn/globebusiness/spring/service/EventInvitationServiceImpl.java
+++ src/main/java/tn/globebusiness/spring/service/EventInvitationServiceImpl.java
@@ -8,14 +8,15 @@ import tn.globebusiness.spring.entity.EventInvitation;
 import tn.globebusiness.spring.repository.EventInvitationRepository;
 
 public class EventInvitationServiceImpl implements IEventInvitationService {
-	
+
 	@Autowired
 	EventInvitationRepository invitationRepository;
 
 	@Override
 	public EventInvitation addInvitation(EventInvitation invitation) {
 		List<EventInvitation> listOfInvitations = listAllInvitations();
-		if(!listOfInvitations.contains(invitation)) return invitationRepository.save(invitation);
+		if (!listOfInvitations.contains(invitation))
+			return invitationRepository.save(invitation);
 		return null;
 	}
 
@@ -27,18 +28,21 @@ public class EventInvitationServiceImpl implements IEventInvitationService {
 	@Override
 	public void deleteInvitation(int invitationId) {
 		invitationRepository.delete(invitationRepository.findById(invitationId).get());
+		
+		
 	}
 
 	@Override
 	public EventInvitation listInvitationByState(String state) {
 		EventInvitation invitation = invitationRepository.findByState(state);
-		if(invitation != null) return invitation;
+		if (invitation != null)
+			return invitation;
 		return null;
 	}
 
 	@Override
 	public List<EventInvitation> listAllInvitations() {
 		return (List<EventInvitation>) invitationRepository.findAll();
-	}
+	}	
 
 }
